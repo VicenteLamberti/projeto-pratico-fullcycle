c141d5a908925b736a6312dee8c96c8480f30740
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-01-18 12:26:38 -0300

    profile test

--- infrastructure/src/test/java/com/fullcycle/vicente/infrastructure/MainTest.java
+++ infrastructure/src/test/java/com/fullcycle/vicente/infrastructure/MainTest.java
@@ -3,11 +3,13 @@ package com.fullcycle.vicente.infrastructure;
 import com.fullcycle.vicente.application.UseCase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
 
 public class MainTest {
 
     @Test
     public void mainTest(){
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME,"test");
         Assertions.assertNotNull(new Main());
         Main.main(new String[]{});
     }
