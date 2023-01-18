87d68eed37a3ce3f4a186cb5cf1cc9e8dbc6ec70
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-01-18 10:50:32 -0300

    Adicionando mysql com docker

--- infrastructure/src/main/java/com/fullcycle/vicente/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/vicente/infrastructure/Main.java
@@ -1,11 +1,15 @@
 package com.fullcycle.vicente.infrastructure;
 
 import com.fullcycle.vicente.application.UseCase;
+import com.fullcycle.vicente.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println(new UseCase().execute());
+        SpringApplication.run(WebServerConfig.class,args);
     }
 }
--- infrastructure/src/main/java/com/fullcycle/vicente/infrastructure/configuration/WebServerConfig.java
+++ infrastructure/src/main/java/com/fullcycle/vicente/infrastructure/configuration/WebServerConfig.java
@@ -1,2 +1,9 @@
-package com.fullcycle.vicente.infrastructure.configuration;public class WebServerConfig {
+package com.fullcycle.vicente.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fullcycle.vicente")
+public class WebServerConfig {
 }
