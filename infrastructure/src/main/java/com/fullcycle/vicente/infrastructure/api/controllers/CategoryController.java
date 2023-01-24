a96c2cd230809eaa594e558498f5127ac9186e37
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-01-24 15:14:32 -0300

    Inicio da API

---
+++ infrastructure/src/main/java/com/fullcycle/vicente/infrastructure/api/controllers/CategoryController.java
@@ -0,0 +1,19 @@
+package com.fullcycle.vicente.infrastructure.api.controllers;
+
+import com.fullcycle.vicente.domain.pagination.Pagination;
+import com.fullcycle.vicente.infrastructure.api.CategoryAPI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController implements CategoryAPI {
+    @Override
+    public ResponseEntity<?> createCategory() {
+        return null;
+    }
+
+    @Override
+    public Pagination<?> listCategories(String search, int page, int perPage, String sort, String direction) {
+        return null;
+    } 
+}
