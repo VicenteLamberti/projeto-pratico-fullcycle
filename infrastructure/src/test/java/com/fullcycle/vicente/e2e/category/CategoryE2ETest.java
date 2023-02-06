ec7690d1b388b04541426a16b1ba060e5803ff0b
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-02-06 16:33:05 -0300

    E2E list e get categories

--- infrastructure/src/test/java/com/fullcycle/vicente/e2e/category/CategoryE2ETest.java
+++ infrastructure/src/test/java/com/fullcycle/vicente/e2e/category/CategoryE2ETest.java
@@ -1,12 +1,13 @@
 package com.fullcycle.vicente.e2e.category;
 
-import com.fullcycle.vicente.ControllerTest;
 import com.fullcycle.vicente.E2ETest;
 import com.fullcycle.vicente.domain.category.CategoryID;
 import com.fullcycle.vicente.infrastructure.category.models.CategoryResponse;
 import com.fullcycle.vicente.infrastructure.category.models.CreateCategoryRequest;
+import com.fullcycle.vicente.infrastructure.category.persistence.CategoryJPAEntity;
 import com.fullcycle.vicente.infrastructure.category.persistence.CategoryRepository;
 import com.fullcycle.vicente.infrastructure.configuration.json.Json;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -71,6 +73,146 @@ public class CategoryE2ETest {
         Assertions.assertNotNull(actualCategory.createdAt());
         Assertions.assertNotNull(actualCategory.updatedAt());
         Assertions.assertNull(actualCategory.deletedAt());
+    }
+
+    @Test
+    public void AsACatalogAdminIsShouldBeAbleToNavigateToAllCategories() throws Exception {
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0,categoryRepository.count());
+
+        givenACategory("Filmes",null,true);
+        givenACategory("Documentarios",null,true);
+        givenACategory("Séries",null,true);
+
+        listCategories(0,1)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.current_page", Matchers.equalTo(0)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items", Matchers.hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items[0].name", Matchers.equalTo("Documentarios")));
+
+        listCategories(1,1)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.current_page", Matchers.equalTo(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items", Matchers.hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items[0].name", Matchers.equalTo("Filmes")));
+
+        listCategories(2,1)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.current_page", Matchers.equalTo(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items", Matchers.hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items[0].name", Matchers.equalTo("Séries")));
+
+        listCategories(3,1)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.current_page", Matchers.equalTo(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items", Matchers.hasSize(0)));
+    }
+
+    @Test
+    public void AsACatalogAdminIsShouldBeAbleToSearchBetweenToAllCategories() throws Exception {
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0,categoryRepository.count());
+
+        givenACategory("Filmes",null,true);
+        givenACategory("Documentarios",null,true);
+        givenACategory("Séries",null,true);
+
+        listCategories(0,1,"fil")
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.current_page", Matchers.equalTo(0)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total", Matchers.equalTo(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items", Matchers.hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items[0].name", Matchers.equalTo("Filmes")));
+
+
+    }
+
+    @Test
+    public void AsACatalogAdminIsShouldBeAbleToSortAllCategoriesByDescriptionDesc() throws Exception {
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0,categoryRepository.count());
+
+        givenACategory("Filmes","C",true);
+        givenACategory("Documentarios","Z",true);
+        givenACategory("Séries","A",true);
+
+        listCategories(0,3,"","description","desc")
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.current_page", Matchers.equalTo(0)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.per_page", Matchers.equalTo(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items", Matchers.hasSize(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items[0].name", Matchers.equalTo("Documentarios")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items[1].name", Matchers.equalTo("Filmes")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.items[2].name", Matchers.equalTo("Séries")));
+
+
+    }
+
+
+    @Test
+    public void asACatalogAdminIsShouldBeAbleToGetACategoryByItsIdentifier() throws Exception {
+
+        final String expectedName = "Filmes";
+        final String expectedDescription = "A categoria mais assistida";
+        final boolean expectedIsActive = true;
+
+        Assertions.assertEquals(0,categoryRepository.count());
+        final CategoryID actualId = givenACategory(expectedName,expectedDescription,expectedIsActive);
+        final CategoryJPAEntity actualCategory = categoryRepository.findById(actualId.getValue()).get();
+
+
+        Assertions.assertEquals(expectedName,actualCategory.getName());
+        Assertions.assertEquals(expectedDescription,actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive,actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    public void asACatalogAdminIsShouldBeAbleToSeeATretatedErrorByGettingNotFound() throws Exception {
+        final MockHttpServletRequestBuilder aRequest = MockMvcRequestBuilders
+                .get("/categories/123")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(aRequest)
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message",Matchers.equalTo("Category with ID 123 was not found")));
+
+    }
+
+
+
+    private ResultActions listCategories(final int page, final int perPage, String search) throws Exception {
+        return listCategories(page,perPage,search,"","");
+    }
+    private ResultActions listCategories(final int page, final int perPage) throws Exception {
+        return listCategories(page,perPage,"","","");
+    }
+    private ResultActions listCategories(final int page, final int perPage, final String search, final String sort, final String direction) throws Exception {
+
+        final MockHttpServletRequestBuilder aRequest = MockMvcRequestBuilders
+                .get("/categories")
+                .queryParam("page",String.valueOf(page))
+                .queryParam("perPage",String.valueOf(perPage))
+                .queryParam("search",search)
+                .queryParam("sort",sort)
+                .queryParam("dir",direction)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        return this.mvc.perform(aRequest);
     }
 
     private CategoryID givenACategory(final String aName, final String aDescription, final boolean isActive) throws Exception {
